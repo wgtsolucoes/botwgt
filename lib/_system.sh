55390d54f76dc46e5819593fe6d580b287ee95f4
Rogério Araújo <rogerio@wgt.net.br>
2026-07-24 17:46:38 -0300

    _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -1,241 +1,581 @@
 #!/bin/bash
+# 
+# system management
 
-get_mysql_root_password() {
-  
+#######################################
+# creates user
+# Arguments:
+#   None
+#######################################
+system_create_user() {
   print_banner
-  printf "${WHITE} 💻 Insira senha para o usuario Deploy e Banco de Dados (Não utilizar caracteres especiais):${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Agora, vamos criar o usuário para a nova Instância...${GRAY_LIGHT}"
   printf "\n\n"
-  read -p "> " mysql_root_password
+
+  sleep 2
+
+  sudo su - root <<EOF
+  useradd -m -p $(openssl passwd -6 ${mysql_root_password}) -s /bin/bash -G sudo deploy
+  usermod -aG sudo deploy
+EOF
+
+  sleep 2
 }
 
-#get_link_git() {
-#  
-#  print_banner
-#  printf "${WHITE} 💻 Insira o link do GITHUB do Multizap que deseja instalar:${GRAY_LIGHT}"
-#  printf "\n\n"
-#  read -p "> " link_git
-#}
-
-get_instancia_add() {
-  
+system_mv_folder() {
   print_banner
-  printf "${WHITE} 💻 Informe um nome para a Instância/Empresa que será instalada (Não utilizar espaços ou caracteres especiais; utilizar letras minusculas):${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Fazendo download do código Multizap...${GRAY_LIGHT}"
   printf "\n\n"
-  read -p "> " instancia_add
+
+  sleep 2
+
+  sudo su - root <<EOF
+  cp "${PROJECT_ROOT}"/Multizap.zip /home/deploy/${instancia_add}/
+EOF
+
+  sleep 2
 }
 
-get_max_whats() {
-  
+#######################################
+# creates folder
+# Arguments:
+#   None
+#######################################
+system_create_folder() {
   print_banner
-  printf "${WHITE} 💻 Informe a Qtde de Conexões/Whats que a ${instancia_add} poderá cadastrar:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Agora, vamos criar a nova pasta da Instância...${GRAY_LIGHT}"
   printf "\n\n"
-  read -p "> " max_whats
+
+  sleep 2
+
+  sudo su - deploy <<EOF 
+  mkdir ${instancia_add}
+EOF
+
+  sleep 2
 }
 
-get_max_user() {
-  
+#######################################
+# unzip whaticket
+# Arguments:
+#   None
+#######################################
+system_unzip_Multizap() {
   print_banner
-  printf "${WHITE} 💻 Informe a Qtde de Usuarios/Atendentes que a ${instancia_add} poderá cadastrar:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Extraindo o Multizap...${GRAY_LIGHT}"
   printf "\n\n"
-  read -p "> " max_user
+
+  sleep 2
+
+  sudo su - deploy <<EOF
+  unzip /home/deploy/${instancia_add}/Multizap.zip -d /home/deploy/${instancia_add}
+EOF
+
+  sudo chmod -R 777 /home/deploy/${instancia_add}/backend/public/
+  sleep
 }
 
-get_frontend_url() {
-  
+#######################################
+# updates system
+# Arguments:
+#   None
+#######################################
+system_update() {
   print_banner
-  printf "${WHITE} 💻 Digite o domínio do FRONTEND/PAINEL para a ${instancia_add}:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos atualizar o sistema Multizap...${GRAY_LIGHT}"
   printf "\n\n"
-  read -p "> " frontend_url
+
+  sleep 2
+
+  sudo su - root <<EOF
+  apt -y update
+  sudo apt-get install -y libxshmfence-dev libgbm-dev wget unzip fontconfig locales gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils
+EOF
+
+  sleep 2
 }
 
-get_backend_url() {
-  
+
+
+#######################################
+# delete system
+# Arguments:
+#   None
+#######################################
+deletar_tudo() {
   print_banner
-  printf "${WHITE} 💻 Digite o domínio do BACKEND/API para a ${instancia_add}:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos deletar uma Instância do Multizap...${GRAY_LIGHT}"
   printf "\n\n"
-  read -p "> " backend_url
-}
 
-get_frontend_port() {
+  sleep 2
+
+  sudo su - root <<EOF
+  docker container rm redis-${empresa_delete} --force
+  cd && rm -rf /etc/nginx/sites-enabled/${empresa_delete}-frontend
+  cd && rm -rf /etc/nginx/sites-enabled/${empresa_delete}-backend  
+  cd && rm -rf /etc/nginx/sites-available/${empresa_delete}-frontend
+  cd && rm -rf /etc/nginx/sites-available/${empresa_delete}-backend
   
+  sleep 2
+
+  sudo su - postgres
+  dropuser ${empresa_delete}
+  dropdb ${empresa_delete}
+  exit
+EOF
+
+sleep 2
+
+sudo su - deploy <<EOF
+ rm -rf /home/deploy/${empresa_delete}
+ pm2 delete ${empresa_delete}-frontend ${empresa_delete}-backend
+ pm2 save
+EOF
+
+  sleep 2
+
   print_banner
-  printf "${WHITE} 💻 Digite a porta do FRONTEND para a ${instancia_add}; Ex: 3000 A 3999 ${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Remoção da Instância/Empresa ${empresa_delete} realizado com sucesso ...${GRAY_LIGHT}"
   printf "\n\n"
-  read -p "> " frontend_port
+
+
+  sleep 2
+
+}
+
+#######################################
+# bloquear system
+# Arguments:
+#   None
+#######################################
+configurar_bloqueio() {
+  print_banner
+  printf "${WHITE} 💻 Vamos Bloquear o Multizap...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+sudo su - deploy <<EOF
+ pm2 stop ${empresa_bloquear}-backend
+ pm2 save
+EOF
+
+  sleep 2
+
+  print_banner
+  printf "${WHITE} 💻 Bloqueio da Instância/Empresa ${empresa_bloquear} realizado com sucesso ...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
 }
 
 
-get_backend_port() {
+#######################################
+# desbloquear system
+# Arguments:
+#   None
+#######################################
+configurar_desbloqueio() {
+  print_banner
+  printf "${WHITE} 💻 Vamos Desbloquear o Multizap de uma Instância...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+sudo su - deploy <<EOF
+ pm2 start ${empresa_bloquear}-backend
+ pm2 save
+EOF
+
+  sleep 2
+
+  print_banner
+  printf "${WHITE} 💻 Desbloqueio da Instância/Empresa ${empresa_desbloquear} realizado com sucesso ...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+}
+
+#######################################
+# alter dominio system
+# Arguments:
+#   None
+#######################################
+configurar_dominio() {
+  print_banner
+  printf "${WHITE} 💻 Vamos alterar os domínios do Multizap de uma Instância...${GRAY_LIGHT}"
+  printf "\n\n"
+
+sleep 2
+
+  sudo su - root <<EOF
+  cd && rm -rf /etc/nginx/sites-enabled/${empresa_dominio}-frontend
+  cd && rm -rf /etc/nginx/sites-enabled/${empresa_dominio}-backend  
+  cd && rm -rf /etc/nginx/sites-available/${empresa_dominio}-frontend
+  cd && rm -rf /etc/nginx/sites-available/${empresa_dominio}-backend
+EOF
+
+sleep 2
+
+  sudo su - deploy <<EOF
+  cd && cd /home/deploy/${empresa_dominio}/frontend
+  sed -i "1c\REACT_APP_BACKEND_URL=https://${alter_backend_url}" .env
+  cd && cd /home/deploy/${empresa_dominio}/backend
+  sed -i "2c\BACKEND_URL=https://${alter_backend_url}" .env
+  sed -i "3c\FRONTEND_URL=https://${alter_frontend_url}" .env 
+EOF
+
+sleep 2
+   
+   backend_hostname=$(echo "${alter_backend_url/https:\/\/}")
+
+ sudo su - root <<EOF
+  cat > /etc/nginx/sites-available/${empresa_dominio}-backend << 'END'
+server {
+  server_name $backend_hostname;
+  location / {
+    proxy_pass http://127.0.0.1:${alter_backend_port};
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection 'upgrade';
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_cache_bypass \$http_upgrade;
+  }
+}
+END
+ln -s /etc/nginx/sites-available/${empresa_dominio}-backend /etc/nginx/sites-enabled
+EOF
+
+sleep 2
+
+frontend_hostname=$(echo "${alter_frontend_url/https:\/\/}")
+
+sudo su - root << EOF
+cat > /etc/nginx/sites-available/${empresa_dominio}-frontend << 'END'
+server {
+  server_name $frontend_hostname;
+  location / {
+    proxy_pass http://127.0.0.1:${alter_frontend_port};
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection 'upgrade';
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_cache_bypass \$http_upgrade;
+  }
+}
+END
+ln -s /etc/nginx/sites-available/${empresa_dominio}-frontend /etc/nginx/sites-enabled
+EOF
+
+ sleep 2
+
+ sudo su - root <<EOF
+  service nginx restart
+EOF
+
+  sleep 2
+
+  backend_domain=$(echo "${backend_url/https:\/\/}")
+  frontend_domain=$(echo "${frontend_url/https:\/\/}")
+
+  sudo su - root <<EOF
+  certbot -m $deploy_email \
+          --nginx \
+          --agree-tos \
+          --non-interactive \
+          --domains $backend_domain,$frontend_domain
+EOF
+
+  sleep 2
+
+  print_banner
+  printf "${WHITE} 💻 Alteração de dominio da Instância/Empresa ${empresa_dominio} realizado com sucesso ...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+}
+
+#######################################
+# installs node
+# Arguments:
+#   None
+#######################################
+system_node_install() {
+  print_banner
+  printf "${WHITE} 💻 Instalando nodejs...${GRAY_LIGHT}"
+  printf "\n\n"
+
+  sleep 2
+
+  sudo su - root <<EOF
+  curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+  apt-get install -y nodejs
+  sleep 2
+  npm install -g npm@latest
+  sleep 2
+  install -d /usr/share/postgresql-common/pgdg
+  curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc
+  sh -c 'echo "deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] http://apt.postgresql.org/pub/repos/apt \$(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+  apt-get update -y && apt-get -y install postgresql
+  sleep 2
+  timedatectl set-timezone America/Sao_Paulo
   
+EOF
+
+  sleep 2
+}
+#######################################
+# installs docker
+# Arguments:
+#   None
+#######################################
+system_docker_install() {
   print_banner
-  printf "${WHITE} 💻 Digite a porta do BACKEND para esta instancia; Ex: 4000 A 4999 ${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Instalando docker...${GRAY_LIGHT}"
   printf "\n\n"
-  read -p "> " backend_port
+
+  sleep 2
+
+  sudo su - root <<EOF
+  apt install -y apt-transport-https \
+                 ca-certificates curl \
+                 software-properties-common gnupg
+
+  install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  chmod a+r /etc/apt/keyrings/docker.asc
+
+  echo \
+    "deb [arch=\$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \$(. /etc/os-release && echo "\$VERSION_CODENAME") stable" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  apt update -y
+  apt install -y docker-ce docker-ce-cli containerd.io
+EOF
+
+  sleep 2
 }
 
-get_redis_port() {
-  
+#######################################
+# Ask for file location containing
+# multiple URL for streaming.
+# Globals:
+#   WHITE
+#   GRAY_LIGHT
+#   BATCH_DIR
+#   PROJECT_ROOT
+# Arguments:
+#   None
+#######################################
+system_puppeteer_dependencies() {
   print_banner
-  printf "${WHITE} 💻 Digite a porta do REDIS/AGENDAMENTO MSG para a ${instancia_add}; Ex: 5000 A 5999 ${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Instalando dependências do puppeteer...${GRAY_LIGHT}"
   printf "\n\n"
-  read -p "> " redis_port
+
+  sleep 2
+
+  sudo su - root <<EOF
+  apt-get install -y libxshmfence-dev \
+                      libgbm-dev \
+                      wget \
+                      unzip \
+                      fontconfig \
+                      locales \
+                      gconf-service \
+                      libasound2 \
+                      libatk1.0-0 \
+                      libc6 \
+                      libcairo2 \
+                      libcups2 \
+                      libdbus-1-3 \
+                      libexpat1 \
+                      libfontconfig1 \
+                      libgcc1 \
+                      libgconf-2-4 \
+                      libgdk-pixbuf2.0-0 \
+                      libglib2.0-0 \
+                      libgtk-3-0 \
+                      libnspr4 \
+                      libpango-1.0-0 \
+                      libpangocairo-1.0-0 \
+                      libstdc++6 \
+                      libx11-6 \
+                      libx11-xcb1 \
+                      libxcb1 \
+                      libxcomposite1 \
+                      libxcursor1 \
+                      libxdamage1 \
+                      libxext6 \
+                      libxfixes3 \
+                      libxi6 \
+                      libxrandr2 \
+                      libxrender1 \
+                      libxss1 \
+                      libxtst6 \
+                      ca-certificates \
+                      fonts-liberation \
+                      libappindicator1 \
+                      libnss3 \
+                      lsb-release \
+                      xdg-utils
+EOF
+
+  sleep 2
 }
 
-get_empresa_delete() {
-  
+#######################################
+# installs pm2
+# Arguments:
+#   None
+#######################################
+system_pm2_install() {
   print_banner
-  printf "${WHITE} 💻 Digite o nome da Instância/Empresa que será Deletada (Digite o mesmo nome de quando instalou):${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Instalando pm2...${GRAY_LIGHT}"
   printf "\n\n"
-  read -p "> " empresa_delete
+
+  sleep 2
+
+  sudo su - root <<EOF
+  npm install -g pm2
+
+EOF
+
+  sleep 2
 }
 
-get_empresa_atualizar() {
-  
+#######################################
+# installs snapd
+# Arguments:
+#   None
+#######################################
+system_snapd_install() {
   print_banner
-  printf "${WHITE} 💻 Digite o nome da Instância/Empresa que deseja Atualizar (Digite o mesmo nome de quando instalou):${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Instalando snapd...${GRAY_LIGHT}"
   printf "\n\n"
-  read -p "> " empresa_atualizar
+
+  sleep 2
+
+  sudo su - root <<EOF
+  apt install -y snapd
+  snap install core
+  snap refresh core
+EOF
+
+  sleep 2
 }
 
-get_empresa_bloquear() {
-  
+#######################################
+# installs certbot
+# Arguments:
+#   None
+#######################################
+system_certbot_install() {
   print_banner
-  printf "${WHITE} 💻 Digite o nome da Instância/Empresa que deseja Bloquear (Digite o mesmo nome de quando instalou):${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Instalando certbot...${GRAY_LIGHT}"
   printf "\n\n"
-  read -p "> " empresa_bloquear
+
+  sleep 2
+
+  sudo su - root <<EOF
+  apt-get remove -y certbot || true
+  snap install --classic certbot
+  ln -sf /snap/bin/certbot /usr/bin/certbot
+EOF
+
+  sleep 2
 }
 
-get_empresa_desbloquear() {
-  
+#######################################
+# installs nginx
+# Arguments:
+#   None
+#######################################
+system_nginx_install() {
   print_banner
-  printf "${WHITE} 💻 Digite o nome da Instância/Empresa que deseja Desbloquear (Digite o mesmo nome de quando instalou):${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Instalando nginx...${GRAY_LIGHT}"
   printf "\n\n"
-  read -p "> " empresa_desbloquear
+
+  sleep 2
+
+  sudo su - root <<EOF
+  apt install -y nginx
+  rm /etc/nginx/sites-enabled/default
+EOF
+
+  sleep 2
 }
 
-get_empresa_dominio() {
-  
+#######################################
+# restarts nginx
+# Arguments:
+#   None
+#######################################
+system_nginx_restart() {
   print_banner
-  printf "${WHITE} 💻 Digite o nome da Instância/Empresa que deseja Alterar os Dominios (Atenção para alterar os dominios precisa digitar os 2, mesmo que vá alterar apenas 1):${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Reiniciando nginx...${GRAY_LIGHT}"
   printf "\n\n"
-  read -p "> " empresa_dominio
+
+  sleep 2
+
+  sudo su - root <<EOF
+  service nginx restart
+EOF
+
+  sleep 2
 }
 
-get_alter_frontend_url() {
-  
+#######################################
+# setup for nginx.conf
+# Arguments:
+#   None
+#######################################
+system_nginx_conf() {
   print_banner
-  printf "${WHITE} 💻 Digite o NOVO domínio do FRONTEND/PAINEL para a ${empresa_dominio}:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Configurando nginx...${GRAY_LIGHT}"
   printf "\n\n"
-  read -p "> " alter_frontend_url
+
+  sleep 2
+
+sudo su - root << EOF
+
+cat > /etc/nginx/conf.d/deploy.conf << 'END'
+client_max_body_size 100M;
+END
+
+EOF
+
+  sleep 2
 }
 
-get_alter_backend_url() {
-  
+#######################################
+# installs nginx
+# Arguments:
+#   None
+#######################################
+system_certbot_setup() {
   print_banner
-  printf "${WHITE} 💻 Digite o NOVO domínio do BACKEND/API para a ${empresa_dominio}:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Configurando certbot...${GRAY_LIGHT}"
   printf "\n\n"
-  read -p "> " alter_backend_url
-}
 
-get_alter_frontend_port() {
-  
-  print_banner
-  printf "${WHITE} 💻 Digite a porta do FRONTEND da Instância/Empresa ${empresa_dominio}; A porta deve ser a mesma informada durante a instalação ${GRAY_LIGHT}"
-  printf "\n\n"
-  read -p "> " alter_frontend_port
-}
+  sleep 2
 
+  backend_domain=$(echo "${backend_url/https:\/\/}")
+  frontend_domain=$(echo "${frontend_url/https:\/\/}")
 
-get_alter_backend_port() {
-  
-  print_banner
-  printf "${WHITE} 💻 Digite a porta do BACKEND da Instância/Empresa ${empresa_dominio}; A porta deve ser a mesma informada durante a instalação ${GRAY_LIGHT}"
-  printf "\n\n"
-  read -p "> " alter_backend_port
-}
+  sudo su - root <<EOF
+  certbot -m $deploy_email \
+          --nginx \
+          --agree-tos \
+          --non-interactive \
+          --domains $backend_domain,$frontend_domain
 
+EOF
 
-get_urls() {
-  get_mysql_root_password
-  get_instancia_add
-  get_max_whats
-  get_max_user
-  get_frontend_url
-  get_backend_url
-  get_frontend_port
-  get_backend_port
-  get_redis_port
-}
-
-software_update() {
-  get_empresa_atualizar
-  frontend_update
-  backend_update
-}
-
-software_delete() {
-  get_empresa_delete
-  deletar_tudo
-}
-
-software_bloquear() {
-  get_empresa_bloquear
-  configurar_bloqueio
-}
-
-software_desbloquear() {
-  get_empresa_desbloquear
-  configurar_desbloqueio
-}
-
-software_dominio() {
-  get_empresa_dominio
-  get_alter_frontend_url
-  get_alter_backend_url
-  get_alter_frontend_port
-  get_alter_backend_port
-  configurar_dominio
-}
-
-inquiry_options() {
-  
-  print_banner
-  printf "${WHITE} 💻 Bem vindo(a) ao Gerenciador Multizap, selecione abaixo a proxima ação!${GRAY_LIGHT}"
-  printf "\n\n"
-  printf "   [0] Instalar Multizap\n"
-  printf "   [1] Atualizar Multizap\n"
-  printf "   [2] Deletar Multizap\n"
-  printf "   [3] Bloquear Multizap\n"
-  printf "   [4] Desbloquear Multizap\n"
-  printf "   [5] Alter. dominio Multizap\n"
-  printf "\n"
-  read -p "> " option
-
-  case "${option}" in
-    0) get_urls ;;
-
-    1) 
-      software_update 
-      exit
-      ;;
-
-    2) 
-      software_delete 
-      exit
-      ;;
-    3) 
-      software_bloquear 
-      exit
-      ;;
-    4) 
-      software_desbloquear 
-      exit
-      ;;
-    5) 
-      software_dominio 
-      exit
-      ;;        
-
-    *) exit ;;
-  esac
+  sleep 2
 }
